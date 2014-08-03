09f49f86dc0740c08ce0dae7ad749a1638c72061
nikoloss <rowland.lancer@gmail.com>
2014-08-03 20:52:00 +0800

    clear

--- PyKTTProject/biz/test.py
+++
@@ -1,6 +0,0 @@
-from util.route import Router
-
-class Test(object):
-    @Router.route(url = r"me/world", method = Router._GET|Router._POST)
-    def test_hello(self,handler):
-        return "hello world" 
