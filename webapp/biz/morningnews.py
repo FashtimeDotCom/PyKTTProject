f583e1780bdcc18087b5f591b2d8e99d55c88fe3
wusezhang <wuse_zjj@163.com>
2014-12-19 14:28:02 +0800

    脚本编辑

--- webapp/biz/morningnews.py
+++ webapp/biz/morningnews.py
@@ -18,12 +18,27 @@ class  MorningNewsResourceAction(object):
         currentdata = {'data':data,'count':count['COUNTS']}
         return req.ok(currentdata)
 
+    #获取国外当天的财经新闻接口#
+    @Router.route(url = r"morningnews/europenews", method = Router._GET|Router._POST)
+    def europe_morningnews_action(self,req):
+        start=req.json_args.get("start")
+        limit=req.json_args.get("limit")
+        type = 'EUROPE'
+        count = self.common_morningnews_count(type)
+        data = self.common_morningnews_data(type,start,limit)
+        currentdata = {'data':data,'count':count['COUNTS']}
+        return req.ok(currentdata)
 
-
-
-
-
-
+    #获取股票当天的财经新闻接口#
+    @Router.route(url = r"morningnews/stocknews", method = Router._GET|Router._POST)
+    def stock_morningnews_action(self,req):
+        start=req.json_args.get("start")
+        limit=req.json_args.get("limit")
+        type = 'STOCK'
+        count = self.common_morningnews_count(type)
+        data = self.common_morningnews_data(type,start,limit)
+        currentdata = {'data':data,'count':count['COUNTS']}
+        return req.ok(currentdata)
 
     #查询财经新闻详情通用查询接口#
     def common_morningnews_data(self,type,start,limit):
