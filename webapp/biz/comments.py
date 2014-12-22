bc69448e6305df2728466bc8a4a0d24083b9b709
wusezhang <wuse_zjj@163.com>
2014-12-22 11:46:21 +0800

    脚本编辑

--- webapp/biz/comments.py
+++ webapp/biz/comments.py
@@ -22,17 +22,17 @@ class  CommentsResourceAction(object):
     #查询当天股票评论详情通用查询接口#
     def daily_stock_comments_data(self,start,limit):
         session = Session('master')
-        SQL = "SELECT  COMMENTSSTOCK.KEYID AS KEYID ," \
-              " COMMENTSSTOCK.TITLE AS TITLE , " \
-              " COMMENTSSTOCK.DESCRIPTCONTEXT AS DESCRIPTCONTEXT , " \
-              " COMMENTSSTOCK.LINKURL AS LINKURL, " \
-              " SUBSTRING(COMMENTSSTOCK.PUBDATE,1,16) AS PUBDATE " \
+        SQL = "SELECT  COMMENTSSTOCK.KEYID AS keyId ," \
+              " COMMENTSSTOCK.TITLE AS title , " \
+              " COMMENTSSTOCK.DESCRIPTCONTEXT AS descriptContext , " \
+              " COMMENTSSTOCK.LINKURL AS linkUrl, " \
+              " SUBSTRING(COMMENTSSTOCK.PUBDATE,1,16) AS pubDate " \
               " FROM  COMMENTS_STOCK_RESOURCE_TABLE  COMMENTSSTOCK " \
               " WHERE 1=1 " \
               " AND   COMMENTSSTOCK.DESCRIPTCONTEXT !='' " \
               " ORDER BY  COMMENTSSTOCK.PUBDATE DESC " \
               " LIMIT %s,%s"%(start,limit)
-        logger.info('查询财经新闻详情通用查询接口...！SQL:'+SQL)
+        logger.info('查询当天股票评论详情通用查询接口...！SQL:'+SQL)
         result = session.select_result(SQL)
         return result
 
@@ -45,3 +45,42 @@ class  CommentsResourceAction(object):
         result = session.select_resultone(SQL)
         return result
 
+
+    #获取当天的财经评论接口--查询当天外汇评论#
+    @Router.route(url = r"comments/dailyfinance", method = Router._GET|Router._POST)
+    def daily_finance_comments_action(self,req):
+        start=req.json_args.get("start")
+        limit=req.json_args.get("limit")
+        count = self.daily_finance_comments_count()
+        data = self.daily_finance_comments_data(start,limit)
+        currentdata = {'data':data,'count':count['COUNTS']}
+        return req.ok(currentdata)
+
+    #查询当天外汇评论详情通用查询接口#
+    def daily_finance_comments_data(self,start,limit):
+        session = Session('master')
+        SQL = " SELECT  COMMENTSFINANCE.KEYID AS keyId , " \
+              " COMMENTSFINANCE.TITLE AS title , " \
+              " COMMENTSFINANCE.DESCRIPTCONTEXT AS descriptContext ," \
+              " COMMENTSFINANCE.LINKURL AS linkUrl, " \
+              " SUBSTRING(COMMENTSFINANCE.PUBDATE,1,16) AS pubDate " \
+              " FROM  COMMENTS_FINANCE_RESOURCE_TABLE  COMMENTSFINANCE" \
+              " WHERE 1=1 " \
+              " AND COMMENTSFINANCE.DESCRIPTCONTEXT !='' " \
+              " AND   COMMENTSFINANCE.COMMENTFLAG = 'FINANCE' " \
+              " ORDER BY  COMMENTSFINANCE.PUBDATE DESC " \
+              " LIMIT %s,%s"%(start,limit)
+        logger.info('查询当天外汇评论详情通用查询接口...！SQL:'+SQL)
+        result = session.select_result(SQL)
+        return result
+
+    #查询当天外汇评论总条数查询接口#
+    def daily_finance_comments_count(self):
+        session = Session('master')
+        SQL =" SELECT  COUNT(*) " \
+             " FROM  COMMENTS_FINANCE_RESOURCE_TABLE  COMMENTSFINANCE " \
+             " WHERE 1=1" \
+             " AND   COMMENTSFINANCE.DESCRIPTCONTEXT !=''" \
+             " AND   COMMENTSFINANCE.COMMENTFLAG = 'FINANCE'"
+        result = session.select_resultone(SQL)
+        return result
