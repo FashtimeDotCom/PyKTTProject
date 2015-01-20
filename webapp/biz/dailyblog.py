5d89d1d02cb7a90e27a173255133043b9b6eca78
wusezhang <wuse_zjj@163.com>
2015-01-20 11:43:20 +0800

    脚本编辑

--- webapp/biz/dailyblog.py
+++ webapp/biz/dailyblog.py
@@ -1,1 +1,13 @@
+#encoding=utf-8
+from util.route import Router
+from db_util import Session
+from util.tools import Log
 
+logger = Log().getLog()
+
+class  DailyBlogResourceAction(object):
+
+    #通过blog日子类型查询相应的日志列表#
+    @Router.route(url = r"dailyblog/byresourcetype", method = Router._GET|Router._POST)
+    def byresourcetype_action(self,req):
+        print ''
