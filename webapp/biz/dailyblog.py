61ab7c63629e5e505761cc2707a17baf9912457d
wusezhang <wuse_zjj@163.com>
2015-01-26 11:35:44 +0800

    脚本编辑

--- webapp/biz/dailyblog.py
+++ webapp/biz/dailyblog.py
@@ -13,27 +13,34 @@ class  DailyBlogResourceAction(object):
         start=req.json_args.get("start")
         limit=req.json_args.get("limit")
         bzfl =req.json_args.get("bzfl")
-        data = self.byresourcetype_resource(bzfl,start,limit)
-        count = self.byresourcetype_count(bzfl)
+        bzname =req.json_args.get("bzname").strip()
+        data = self.byresourcetype_resource(bzfl,start,limit,bzname)
+        count = self.byresourcetype_count(bzfl,bzname)
         logger.info(count['COUNTS'])
         currentdata = {'data':data,'count':count['COUNTS']}
         return req.ok(currentdata)
 
-    def byresourcetype_resource(self,bzfl,start,limit):
+    def byresourcetype_resource(self,bzfl,start,limit,bzname):
         session = Session('master')
+        SQL =None
         SQL = " SELECT A.BZ_NAME AS bzname, A.BZ_INTRODUCE AS bzintroduce, A.BZ_FL AS bzfl," \
               " A.SRC_NAME AS srcname, A.ID AS id " \
               " FROM DAILYBLOG_AUTHOR_RESOURCE_TABLE A WHERE 1 = 1" \
-              " AND A.BZ_FL=%s LIMIT %s,%s"%(bzfl,start,limit)
+              " AND A.BZ_FL=%s "%(bzfl)
+        if(''!=bzname):
+           SQL +="AND A.BZ_NAME LIKE %"+bzname+"%"
+        SQL += " ORDER BY  POPULATION DESC  LIMIT %s,%s"%(start,limit)
         logger.info('查询财经作者列表信息...！'+SQL)
         resources = session.select_result(SQL)
         return resources
 
-    def byresourcetype_count(self,bzfl):
+    def byresourcetype_count(self,bzfl,bzname):
         session = Session('master')
         SQL = " SELECT COUNT(ID) AS COUNTS " \
               " FROM DAILYBLOG_AUTHOR_RESOURCE_TABLE A WHERE 1 = 1" \
               " AND A.BZ_FL=%s "%(bzfl)
+        if(''!=bzname):
+           SQL +="AND A.BZ_NAME LIKE %"+bzname+"%"
         logger.info('查询财经作者列表信息...！'+SQL)
         resources = session.select_resultone(SQL)
         return resources
