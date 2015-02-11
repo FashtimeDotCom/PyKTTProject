286dd8080626d18fb64cd02345fee049cf559e7b
zhang.jj <wuse_zjj@163.com>
2015-02-11 11:03:17 +0800

    脚本编辑

--- webapp/biz/themecompany.py
+++ webapp/biz/themecompany.py
@@ -7,7 +7,7 @@ logger = Log().getLog()
 
 class ThemeCompanyResourceAction():
 
-    #获取当天的财经评论接口--查询当天股票评论#
+    #获取当天的股票板块信息#
     @Router.route(url = r"themecompany/dailythemesnews", method = Router._GET|Router._POST)
     def daily_news_themes_action(self,req):
         start=req.json_args.get("start")
@@ -34,5 +34,41 @@ class ThemeCompanyResourceAction():
         session = Session('master')
         SQL =" SELECT COUNT(*) AS COUNTS FROM STOCK_POOL_THEME_NEWS_TABLE THEMENEWS" \
              " WHERE 1 = 1"
+        result = session.select_resultone(SQL)
+        return result
+
+
+    #获取当天的上市公司信息信息#
+    @Router.route(url = r"themecompany/dailycompanynews", method = Router._GET|Router._POST)
+    def daily_news_company_action(self,req):
+        start=req.json_args.get("start")
+        limit=req.json_args.get("limit")
+        count = self.daily_news_company_count()
+        data = self.daily_news_company_data(start,limit)
+        currentdata = {'data':data,'count':count['COUNTS']}
+        return req.ok(currentdata)
+
+    def daily_news_company_data(self,start,limit):
+        session = Session('master')
+        SQL = " SELECT MAX(COMPANYNEWS.KEYID) AS KEYID," \
+              " MAX(COMPANYNEWS.LINKURL) AS LINKURL," \
+              " MAX(SUBSTRING(COMPANYNEWS.PUBDATE, 1, 16)) AS PUBDATE," \
+              " MAX(SUBSTRING(COMPANYNEWS.TITLE, 1, 35)) AS TITLE" \
+              " FROM" \
+              " STOCK_POOL_COMPANY_NEWS_TABLE COMPANYNEWS" \
+              " WHERE 1 = 1" \
+              " GROUP BY COMPANYNEWS.TITLE" \
+              " ORDER BY COMPANYNEWS.PUBDATE DESC" \
+              " LIMIT %s,%s"%(start,limit)
+        logger.info('查询当天股票板块信息接口...！SQL:'+SQL)
+        result = session.select_result(SQL)
+        return result
+
+
+    def daily_news_company_count(self):
+        session = Session('master')
+        SQL =" SELECT COUNT(*) AS COUNTS FROM (SELECT MAX(KEYID)" \
+             " FROM STOCK_POOL_COMPANY_NEWS_TABLE COMPANYNEWS" \
+             " WHERE 1 = 1 GROUP BY COMPANYNEWS.TITLE ) AS COMPANYNEWS "
         result = session.select_resultone(SQL)
         return result
